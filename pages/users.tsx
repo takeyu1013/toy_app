8f676415bfa3c5185928ee8d553667f2151ad85e
takeyu1013 <yuto1013@gmail.com>
2021-12-02 23:07:51 +0900

    ✨ Add to show users

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,7 +1,23 @@
-import type { NextPage } from "next";
+import type { NextPage, GetServerSideProps } from "next";
 import Link from "next/link";
 
-const Users: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch("http://localhost:3001/users");
+  const users = await res.json();
+  return {
+    props: { users },
+  };
+};
+
+type User = {
+  id: number;
+  email: string;
+  name: string;
+};
+
+type Props = { users: User[] };
+
+const Users: NextPage<Props> = (props) => {
   return (
     <div>
       <h1>Users</h1>
@@ -11,7 +27,31 @@ const Users: NextPage = () => {
           <th>Email</th>
           <th colSpan={3}></th>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {props.users.map((user) => {
+            return (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <Link href={`/user/${user.id}`}>
+                    <a>Show</a>
+                  </Link>
+                </td>
+                <td>
+                  <Link href={`/user/${user.id}/edit`}>
+                    <a>Edit</a>
+                  </Link>
+                </td>
+                <td>
+                  <Link href="">
+                    <a>Destroy</a>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
       <Link href="/users/new">
         <a>New User</a>
