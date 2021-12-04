168a3b4dc5873e65595022121fc056a1bfb5eb27
takeyu1013 <yuto1013@gmail.com>
2021-12-04 22:48:25 +0900

    🐛 Fix type error

--- pages/users/new.tsx
+++ pages/users/new.tsx
@@ -1,22 +1,29 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 const New: NextPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const handleName = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setName(event.target.value);
+    },
+    []
+  );
+  const handleEmail = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(event.target.value);
+    },
+    []
+  );
   const createUser = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(
-      JSON.stringify({
-        name: event.target.name.value,
-        email: event.target.email.value,
-      })
-    );
-    console.log(`${process.env.NEXT_PUBLIC_BACKEND_HOST}/user`);
 
     await fetch(`${process.env.NEXT_PUBLIC_BACKEND_HOST}/user`, {
       body: JSON.stringify({
-        name: event.target.name.value,
-        email: event.target.email.value,
+        name: name,
+        email: email,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -30,11 +37,25 @@ const New: NextPage = () => {
       <form onSubmit={createUser}>
         <div className="field">
           <label htmlFor="name">Name</label>
-          <input id="name" type="text" autoComplete="name" required />
+          <input
+            id="name"
+            type="text"
+            autoComplete="name"
+            required
+            value={name}
+            onChange={handleName}
+          />
         </div>
         <div className="field">
           <label htmlFor="email">Email</label>
-          <input id="email" type="text" autoComplete="email" required />
+          <input
+            id="email"
+            type="text"
+            autoComplete="email"
+            required
+            value={email}
+            onChange={handleEmail}
+          />
         </div>
         <div className="actions">
           <button type="submit">Create User</button>
