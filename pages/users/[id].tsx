85c7b0d9b1e40f7f209778057ededea3dffb6194
takeyu1013 <yuto1013@gmail.com>
2021-12-05 22:08:31 +0900

    👽️ Add to get user info from backend

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,11 +1,41 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
+import Link from "next/dist/client/link";
+import { useRouter } from "next/dist/client/router";
 
-const User: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_HOST}/users/${id}`
+  );
+  const user = await res.json();
+  return {
+    props: { user },
+  };
+};
+
+type User = {
+  id: number;
+  email: string;
+  name: string;
+};
+
+type Props = { user: User };
+
+const User: NextPage<Props> = (props) => {
+  const router = useRouter();
   return (
     <div>
       <p>User was successfully created.</p>
-      <p>Name: {/* props.user.id */}</p>
-      <p>Email: {/* props.user.email */}</p>
+      <p>
+        <strong>Name:</strong> {props.user.name}
+      </p>
+      <p>
+        <strong>Email:</strong> {props.user.email}
+      </p>
+      <Link href={`/users/${router.query}/edit`}>
+        <a>Edit</a>
+      </Link>
+      <Link href="/users">Back</Link>
     </div>
   );
 };
