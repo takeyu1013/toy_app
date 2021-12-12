a0f0c7b351a17acc3d79d6a2012e01cad58c066c
takeyu1013 <yuto1013@gmail.com>
2021-12-12 19:10:56 +0900

    💄 Change default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["verdana", "arial", "helvetica", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
